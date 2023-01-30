ae83426991c468847d05fcb232e42a88bd396522
sneg <sneg.sbg@gmail.com>
2023-01-30 22:50:56 +0300

    working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+Console.Clear();
+
+
+int[] get_array(int min_value, int max_value)
+{
+    Random temp_random = new Random();
+    int size = temp_random.Next(6, 12); 
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = temp_random.Next(min_value, max_value);
+    }
+    return res;
+}
+
+
+int index (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+void main()
+{
+int[] our_array = get_array(-100, 100);
+int ar = index(our_array);
+Console.WriteLine(String.Join(",", our_array));
+Console.WriteLine(ar);
+}
+
+
+
